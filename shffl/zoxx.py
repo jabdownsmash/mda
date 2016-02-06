fd26e4d66970539b7a02d676d6d12ca9402b7da3
Aaron Santiago <aaronsantiago@gmail.com>
2016-02-06 00:58:09 -0500

    Abstract overlay system

--- shffl/zoxx.py
+++ shffl/zoxx.py
@@ -4,11 +4,11 @@ import math
 import random
 from PySide import QtCore, QtGui
 from p3.state_manager import StateManager
-from p3.state import State
+from p3.state import State, Menu
 from p3.memory_watcher import MemoryWatcher
 
 class Container(QtGui.QWidget):
-    def __init__(self, board, quitCB, parent=None):
+    def __init__(self, board, closeCallback, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
@@ -16,42 +16,56 @@ class Container(QtGui.QWidget):
         quit.resize(75, 30)
         quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
 
-        QtCore.QObject.connect(quit, QtCore.SIGNAL("clicked()"),lambda:quitCB())
+        def buttonCB():
+            board.calibrating = 2
+
+        QtCore.QObject.connect(quit, QtCore.SIGNAL("clicked()"),buttonCB)
         # quit.show()
         # board.show()
         gridLayout = QtGui.QGridLayout()
         gridLayout.addWidget(quit, 0, 0)
-        gridLayout.addWidget(board, 1, 1, 2, 1)
+        gridLayout.addWidget(board, 1, 0, 2, 2)
         gridLayout.setColumnStretch(1, 10)
         self.setLayout(gridLayout)
         # self.show()
+        self.ccb = closeCallback
+
+    def closeEvent(self, event):
+        self.ccb()
 
 class Melee(QtGui.QWidget):
-    def __init__(self, mouseCallback,closeCallback, parent=None):
+    def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
         self.rectangles = []
         self.texts = []
         self.kek = 0
-        self.mcb = mouseCallback
-        self.ccb = closeCallback
+
+        self.x1 = 0
+        self.x2 = 100
+        self.y1 = 0
+        self.y2 = 0
+        self.calibrating = 0
 
 
     def paintEvent(self,event):
         painter = QtGui.QPainter(self)
+        scale = (self.x2 - self.x1)/(88.47*2)
+        xOffset = self.x1 + (self.x2 - self.x1)/2
+        yOffset = (self.y1 + self.y2)/2
 
         for text in self.texts:
             painter.setPen(QtCore.Qt.white)
             painter.setFont(QtGui.QFont("Courier", 20, QtGui.QFont.Bold))
             painter.save()
-            painter.translate(text[0], text[1])
+            painter.translate(text[0]*scale + xOffset, text[1]*scale + yOffset)
             painter.drawText(self.rect(), text[2])
             painter.restore()
 
         for rect in self.rectangles:
             painter.setPen(QtCore.Qt.white)
             painter.setBrush(QtGui.QColor(*rect[4]))
-            painter.drawRect(QtCore.QRect(rect[0], rect[1], rect[2], rect[3]))
+            painter.drawRect(QtCore.QRect(rect[0]*scale + xOffset - rect[2]/2, -rect[1]*scale + yOffset - rect[3]/2, rect[2], rect[3]))
 
         # painter.setPen(QtCore.Qt.NoPen)
         # painter.setBrush(QtCore.Qt.blue)
@@ -64,11 +78,17 @@ class Melee(QtGui.QWidget):
         if event.button() == QtCore.Qt.LeftButton:
             x = event.pos().x()
             y = event.pos().y()
-            self.mcb(x,y)
+            print("x: " + str(x) + " y: " + str(y))
+            # self.mcb(x,y)
+            if self.calibrating == 2:
+                self.x1 = x
+                self.y1 = y
+                self.calibrating -= 1
+            elif self.calibrating == 1:
+                self.x2 = x
+                self.y2 = y
+                self.calibrating -= 1
         self.update()
-
-    def closeEvent(self, event):
-        self.ccb()
 
 def listener (state):
     pass
@@ -76,8 +96,7 @@ def listener (state):
     if state.players[0].vertical_velocity < 0 and state.players[0].vertical_velocity > -state.players[0].fastfall_velocity:
         print('hey')
 
-
-if __name__ == '__main__':
+def start(setup):
 
     if len(sys.argv) != 2:
         sys.exit('Usage: ' + sys.argv[0] + ' dolphin-home')
@@ -97,46 +116,17 @@ if __name__ == '__main__':
         file.write(locationsTxt)
 
     done = False
-    x1 = 0
-    x2 = 100
-    y1 = 0
-    y2 = 0
-    calibrating = 0
 
     def exitHandler():
-        global done
+        nonlocal done
         done = True
 
-    def calibrate():
-        global calibrating
-        calibrating = 2
-
-    def mouseHandler(x,y):
-        global calibrating, x1, x2, y1, y2
-        if calibrating == 2:
-            x1 = x
-            y1 = y
-            calibrating -= 1
-        elif calibrating == 1:
-            x2 = x
-            y2 = y
-            calibrating -= 1
-
     app = QtGui.QApplication(sys.argv)
-    board = Melee(mouseHandler, exitHandler)
 
-    cont = Container(board,calibrate)
+    board = Melee()
+    cont = Container(board, exitHandler)
     cont.show()
-
-    kek = [300,400,100,100,(255,0,0,100)]
-    board.rectangles.append(kek)
-
-    def listener2 (state):
-        scale = (x2 - x1)/(88.47*2)
-        kek[0] = state.players[0].x*scale + x1 + (x2 - x1)/2 - 50
-        kek[1] = -state.players[0].y*scale - 50 + (y2 + y1)/2
-        board.update()
-    state.players[0].x_changed.append(listener2)
+    setup(state,board,cont)
     # board.texts.append((200,350,"hello"))
 
     mww = MemoryWatcher(home + '/MemoryWatcher/MemoryWatcher')
@@ -149,4 +139,17 @@ if __name__ == '__main__':
             break
 
 # sys.exit(app.exec_())
+def zoxx(state,board,cont):
+    kek = [300,400,100,100,(255,0,0,60)]
+    board.rectangles.append(kek)
 
+    def listener2 (state):
+        if state.menu == Menu.Game:
+            kek[0] = state.players[0].x
+            kek[1] = state.players[0].y
+            board.update()
+    state.frame_changed.append(listener2)
+    # state.players[0].x_changed.append(listener2)
+
+if __name__ == '__main__':
+    start(zoxx)
